c0c9d9394e22425a3ed32672ca5887d025761eb3
tianzhang <495236549@qq.com>
2018-10-22 17:01:11 +0800

    b

--- Mission/src/store/types.js
+++
@@ -1,8 +0,0 @@
-//新增一条
-export const SAVE_PLAN = 'SAVE_PLAN'
-//减少一条
-export const DEL_PLAN = 'DEL_PLAN'
-//总时间增加
-export const ADD_TOTAL_TIME = 'ADD_TOTAL_TIME'
-//总时间减少
-export const DEC_TOTAL_TIME ='DEC_TOTAL_TIME'
